f110ac424d6dcd3d87b4e61f8f997bb256b59123
Seth Gardner <seth.gardner.a@gmail.com>
2021-07-26 10:39:34 -0500

    nominal changes

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 "use strict";
+const serverUrl = "https://shell-booming-guanaco.glitch.me/movies";
 
 fetch('https://shell-booming-guanaco.glitch.me/movies').then(response =>{
     response.json().then(movies =>{
         console.log(movies)
     });
 });
+
+function Ajax(url,method = "GET", data) {
+    const options = {
+        method: method,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    };
+    return fetch(url)
+        .then(res => res.json())
+        .then(responseData => responseData)
+        .catch(err => err)
+}
+
+    Ajax(serverUrl, method = "POST", {title: " we built an ajax function"})
+.then(function(data){
+    console.log(data)
+})
+
