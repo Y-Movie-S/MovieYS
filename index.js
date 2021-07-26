92976e2711bf25cfb555ca39897b3ee3e08c8c73
Youssef Rivas <Youssef.Rivas@gmail.com>
2021-07-26 11:05:10 -0500

    "starting out"

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
 "use strict";
-const serverUrl = "https://shell-booming-guanaco.glitch.me/movies";
+$(document).ready(function() {
+    const serverUrl = "https://shell-booming-guanaco.glitch.me/movies";
 
-fetch('https://shell-booming-guanaco.glitch.me/movies').then(response =>{
-    response.json().then(movies =>{
-        console.log(movies)
+    fetch('https://shell-booming-guanaco.glitch.me/movies').then(response => {
+        response.json().then(movies => {
+            console.log(movies);
+        $("#card-1").append("string");
+        });
     });
-});
 
-function Ajax(url,method = "GET", data) {
-    const options = {
-        method: method,
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-    };
-    return fetch(url)
-        .then(res => res.json())
-        .then(responseData => responseData)
-        .catch(err => err)
-}
+    function Ajax(url, method = "GET", data) {
+        const options = {
+            method: method,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        };
+        return fetch(url)
+            .then(res => res.json())
+            .then(responseData => responseData)
+            .catch(err => err)
+    }
 
     Ajax(serverUrl, method = "POST", {title: " we built an ajax function"})
-.then(function(data){
-    console.log(data)
+        .then(function (data) {
+            console.log(data)
+        })
 })
 
